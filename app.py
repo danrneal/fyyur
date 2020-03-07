5480705c3c169e3c9dcb4e91b9a681f173b5f6e5
Daniel R. Neal <dan.r.neal@gmail.com>
2020-03-07 12:43:23 -0800

    refactor: Remove unused imports

--- app.py
+++ app.py
@@ -2,19 +2,18 @@
 # Imports
 # ----------------------------------------------------------------------------#
 
-import json
-import dateutil.parser
-import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
-from flask_wtf import Form
-from forms import *
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
+import babel
+import dateutil.parser
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from forms import VenueForm, ArtistForm, ShowForm
+
+# ----------------------------------------------------------------------------#
+# App Config
+# ----------------------------------------------------------------------------#
 
 app = Flask(__name__)
 moment = Moment(app)
