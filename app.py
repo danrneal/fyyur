261beeebf01e54624ae9c2ac1c0bce807ab5407b
Daniel R. Neal <dan.r.neal@gmail.com>
2020-03-07 12:47:28 -0800

    refactor: Rename variable so it no longer shadows a builtin

--- app.py
+++ app.py
@@ -66,16 +66,16 @@ class Artist(db.Model):
 # ----------------------------------------------------------------------------#
 
 
-def format_datetime(value, format='medium'):
+def format_datetime(value, datetime_format='medium'):
     date = dateutil.parser.parse(value)
-    if format == 'full':
-        format = "EEEE MMMM, d, y 'at' h:mma"
-    elif format == 'medium':
-        format = "EE MM, dd, y h:mma"
-    return babel.dates.format_datetime(date, format)
+    if datetime_format == 'full':
+        datetime_format = "EEEE MMMM, d, y 'at' h:mma"
+    elif datetime_format == 'medium':
+        datetime_format = "EE MM, dd, y h:mma"
+    return babel.dates.format_datetime(date, datetime_format)
 
 
-app.jinja_env.filters['datetime'] = format_datetime
+app.jinja_env().filters['datetime'] = format_datetime
 
 # ----------------------------------------------------------------------------#
 # Controllers
