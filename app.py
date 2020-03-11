535700c7388ced3a9cd57d8fe6563d55f7abc681
Daniel R. Neal <dan.r.neal@gmail.com>
2020-03-11 10:27:11 -0700

    feat: Show edit artist form pre-populated w previous data

--- app.py
+++ app.py
@@ -762,26 +762,17 @@ def show_artist(artist_id):
 
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-    form = ArtistForm()
-    artist = {
-        "id": 4,
-        "name": "Guns N Petals",
-        "genres": ["Rock n Roll"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "326-123-5000",
-        "website": "https://www.gunsnpetalsband.com",
-        "facebook_link": "https://www.facebook.com/GunsNPetals",
-        "seeking_venue": True,
-        "seeking_description": (
-            "Looking for shows to perform at in the San Francisco Bay Area!"
-        ),
-        "image_link": (
-            "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=r"
-            "b-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-        )
-    }
-    # TODO: populate form with fields from artist with ID <artist_id>
+    """Displays the form for editing an artist
+
+    Args:
+        artist_id: A str representing the id of the artist to edit
+
+    Returns:
+        A html template for the artist form which has been pre-populated
+    """
+    artist = Artist.query.get(artist_id)
+    form = ArtistForm(obj=artist)
+
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 
