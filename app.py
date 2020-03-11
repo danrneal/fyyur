3fdbeaec4e270fc1f67a2f70dc023ae4f5a1d807
Daniel R. Neal <dan.r.neal@gmail.com>
2020-03-11 10:32:04 -0700

    feat: Show list of artists on artists page

--- app.py
+++ app.py
@@ -520,18 +520,13 @@ def delete_venue(venue_id):
 
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
-    return render_template('pages/artists.html', artists=data)
+    """Route handler for showing a list of artists
+
+    Returns:
+        A template for a list of all artists
+    """
+    artists_all = Artist.query.all()
+    return render_template('pages/artists.html', artists=artists_all)
 
 
 @app.route('/artists/search', methods=['POST'])
