5401398420bf8cc4e5722a05dbfc2f15cdddf316
Daniel R. Neal <dan.r.neal@gmail.com>
2020-07-21 18:49:41 -0700

    docs: Add attributes of module to docstring

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
-"""Config for SQLAlchemy and specifying db params"""
+"""Config for SQLAlchemy and specifying db params.
 
-import os
+Attributes:
+    SECRET_KEY: A str representing a secret key used for securely signing the
+        session cookie
+    basedir: A str represneding the the current path
+    DEBUG: A bool setting the debug config flask app config parameter
+    DIALECT: A str representing the dialect of the db
+    HOST: A str representing the host of the db
+    PORT: An int representing the port the db is running on
+    DATABASE: A str representing the db in which to connect to
+    SQLALCHEMY_DATABASE_URI: A str representing the location of the db
+    SQLALCHEMY_TRACK_MODIFICATIONS: A bool representing the flask app config
+        parameter by the same name
+"""
 
 
 SECRET_KEY = os.urandom(32)
